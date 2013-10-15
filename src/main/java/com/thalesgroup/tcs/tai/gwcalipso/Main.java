852351ae43e6b036b533baf47108e6ec219d0f21
Rémy Léone <remy.leone@gmail.com>
2013-10-15 17:55:39 +0200

    redis_init

--- src/main/java/com/thalesgroup/tcs/tai/gwcalipso/Main.java
+++ src/main/java/com/thalesgroup/tcs/tai/gwcalipso/Main.java
@@ -22,7 +22,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         final HttpServer server = startServer();
-        //Redis.redis_init();
+        Redis.redis_init();
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
         System.in.read();
