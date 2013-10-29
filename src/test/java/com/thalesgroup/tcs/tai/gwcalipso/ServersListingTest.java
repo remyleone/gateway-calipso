a587e458aab5c350a14fd3c7b3e8ac0372c3d2c4
Rémy Léone <remy.leone@gmail.com>
2013-10-29 14:55:23 +0100

    renaming

--- src/test/java/com/thalesgroup/tcs/tai/gwcalipso/ServersListingTest.java
+++ src/test/java/com/thalesgroup/tcs/tai/gwcalipso/ServersListingTest.java
@@ -13,7 +13,7 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
-public class ServersTest {
+public class ServersListingTest {
 
     private HttpServer server;
     private WebTarget target;
